bb8b49b67c491e913f9c33109ffa78518676f424
poojareddy3 <poojithavangala@gmail.com>
2022-03-17 16:14:19 -0400

    deleted files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import Events from './components/Events';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello World!</h1>
+     <Button/>
+     <Events title="Polyphia" date="12/22"/>
+     <Events title="Maroon 5"/>
+     <Events title="Ed Sheeran"/>
+     <Events title="Chief Keif"/>
+     <Events title="Bad Bunny"/>
     </div>
   );
 }
---
+++ src/components/Events.jsx
@@ -0,0 +1,12 @@
+function Events({ title, date }) {  
+   return (
+      <div>
+      <h2>{ title }</h2>
+      <p>Concert</p>
+      <p>{ date }</p>
+       {/* jsx => JavaScript Expressions- when we fuse HTML & JS */}
+      </div>
+   )
+}
+
+export default Events
